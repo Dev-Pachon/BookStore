adf42c5107ae3d8e8b621dbb0d839794faf9b66b
Dev.Pachon <54422629+Dev-Pachon@users.noreply.github.com>
2021-03-19 17:13:43 -0500

    Added Bubblesort algorithm

--- src/model/Bookstore.java
+++ src/model/Bookstore.java
@@ -74,7 +74,7 @@ public class Bookstore {
 			quickSort(clientBooks, 0, clientBooks.length-1);
 			break;
 		case BUBBLESORT:
-			
+			bubbleSort(clientBooks);
 			break;
 		}
 
@@ -109,6 +109,18 @@ public class Bookstore {
 		} 
 	}
 
-
+	private<T extends Comparable<T>> void bubbleSort(T arr[]) 
+	{ 
+		int n = arr.length; 
+		for (int i = 0; i < n-1; i++) 
+			for (int j = 0; j < n-i-1; j++) 
+				if (arr[j].compareTo(arr[j+1])>0) 
+				{ 
+					// swap arr[j+1] and arr[j] 
+					T temp = arr[j]; 
+					arr[j] = arr[j+1]; 
+					arr[j+1] = temp; 
+				} 
+	}
 
 }
