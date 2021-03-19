0b559cb8ead694a501783f983d680cfa54f6e8f2
TheRealFakeness <juanjo312003@gmail.com>
2021-03-18 23:47:51 -0500

    Create Client.java

---
+++ src/model/Client.java
@@ -0,0 +1,29 @@
+package model;
+
+import dataStructures.Stack;
+
+public class Client {
+	private Stack<Book> basket;
+	private int time;
+	
+	public Client() {
+		basket = new Stack()<Book>;
+		
+	}
+	
+	public Stack<Book> getBasket() {
+		return basket;
+	}
+	
+	public void setBasket(Stack<Book> basket) {
+		this.basket = basket;
+	}
+	
+	public int getTime() {
+		return time;
+	}
+	
+	public void setTime(int time) {
+		this.time = time;
+	}
+}
